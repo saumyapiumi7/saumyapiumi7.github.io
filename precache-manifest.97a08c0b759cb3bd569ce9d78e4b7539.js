27cdcb099911aa4c48d8381a5e84645359d73b12
Saumya Herath <saumyapiumi7@gmail.com>
2023-03-16 08:05:51 +0530

    Updates

---
+++ precache-manifest.97a08c0b759cb3bd569ce9d78e4b7539.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b584627cc9b3a5e08e250181545987f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "627b0d24bd5e2e8e812f",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "ee903fcadf3e21eb000d",
+    "url": "/static/css/main.ced2ec41.chunk.css"
+  },
+  {
+    "revision": "627b0d24bd5e2e8e812f",
+    "url": "/static/js/2.6716f6e5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6716f6e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee903fcadf3e21eb000d",
+    "url": "/static/js/main.57013966.chunk.js"
+  },
+  {
+    "revision": "705bd4d60a3db7eea726",
+    "url": "/static/js/runtime-main.dccd1af4.js"
+  },
+  {
+    "revision": "675832e2a635b59cdd8988bed51d0e2a",
+    "url": "/static/media/android.675832e2.png"
+  },
+  {
+    "revision": "2e77a981636a19affd7e375dcdc5e0ca",
+    "url": "/static/media/ios.2e77a981.jpg"
+  },
+  {
+    "revision": "b3c59c773de2b0984ff9014f21c0ae34",
+    "url": "/static/media/pic.b3c59c77.jpg"
+  },
+  {
+    "revision": "bc2f64ff230cbf4b8f6093c574985022",
+    "url": "/static/media/web.bc2f64ff.jpg"
+  }
+]);
